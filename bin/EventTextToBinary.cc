f193ab969d07d1fa62a24dfb13217ceb79ff78c4
Steven Kaplan <skaplan@physics.rutgers.edu>
2014-05-19 12:42:28 -0400

    added event trailers and first event header

--- bin/EventTextToBinary.cc
+++ bin/EventTextToBinary.cc
@@ -6,6 +6,7 @@
 **   <Channel> <ROC> <Column> <Row> <ADC> <Event Number>   **
 **                                                         **
 **              Written by Steven. M Kaplan                **
+**  Relies heavily on bin/PLTMC.cc written by Dean Hidas   **
 **                                                         **
 ************************************************************/
 
@@ -31,15 +32,40 @@ void createBinaryFile(){
     string line;
     vector< vector<int> > eventInfo;
     int currentEventNumber = -10; //negative as a placeholder for the first event
+    int hitCount = 0;
+    bool firstLine = true;
     while( getline(textFile, line) ){
         if (line.size() == 0) continue; //skip empty lines
-        int channel,ROC,column,row,eventNum;
-        double adc;
+        int channel,ROC,column,row,eventNum,adc;
+        double adcdouble;
         uint32_t n,n2;
         //create stringstream object for each line
         stringstream ss(line);
-        ss >> channel >> ROC >> column >> row >> adc >> eventNum;
-        if ( eventNum > currentEventNumber ){ //when starting new event, write new event header to binary
+        ss >> channel >> ROC >> column >> row >> adcdouble >> eventNum;
+        adc = static_cast<int>(adcdouble);
+        if ( eventNum > currentEventNumber ){
+            if (!firstLine){
+                //stuff to write after the previous event
+                if (hitCount % 2 == 0) {
+                    // even number of hits.. need to fill out the word.. just print the number over two as 2x32  words
+                    n  = 0xa0000000;
+                    n2 = 0x00000000;
+                    n  |= (hitCount / 2 + 2);
+                    binaryFile.write( (char*) &n2, sizeof(uint32_t) );
+                    binaryFile.write( (char*) &n, sizeof(uint32_t) );
+                }
+                else {
+                    // Print number of hits in 1x32 bit
+                    n  = 0x00000000;
+                    binaryFile.write( (char*) &n, sizeof(uint32_t) );
+                    binaryFile.write( (char*) &n, sizeof(uint32_t) );
+                    n  = 0xa0000000;
+                    n  |= (hitCount / 2 + 1);
+                    binaryFile.write( (char*) &n, sizeof(uint32_t) );
+                }
+            }
+            //stuff to begin the next event
+            hitCount = 0;
             currentEventNumber = eventNum;
             n2 = (5 << 8);
             n = 0x50000000;
@@ -47,6 +73,7 @@ void createBinaryFile(){
             binaryFile.write( (char*) &n2, sizeof(uint32_t) );
             binaryFile.write( (char*) &n, sizeof(uint32_t) );
         }
+        hitCount++;
         n = 0x00000000;
         n |= (channel << 26);
         n |= ( (ROC + 1) << 21);
@@ -63,9 +90,10 @@ void createBinaryFile(){
             n |= (( (column - 1) / 2) << 16  );
         }
         //casting to force adc to be an int, is this ok? It needs to be in order to do binary operations!
-        n |= (int)adc;
+        n |= adc;
         binaryFile.write( (char*) &n, sizeof(uint32_t) );
         ss.str( string() ); //flush stringstream for next line in the text file
+        firstLine = false;
     }
 
 }
